59593df23ac5aebb8f14b59b90fc0cf8851d90c8
Pankaj Singh <pankaj@redpanthers.co>
2017-06-23 16:13:31 +0530

    Add 404 page, gems

--- config.rb
+++ config.rb
@@ -46,3 +46,8 @@ page "/footer.html", :layout => "layout"
 #   activate :minify_css
 #   activate :minify_javascript
 # end
+# Use “pretty” URLs (without the `.html` suffix)
+activate :directory_indexes
+
+# Append hashes to compiled assets
+activate :asset_hash
